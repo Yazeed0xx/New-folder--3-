0b3347e3d293c28aa8d63d448e6788e6937c3d4d
Yazeed0xx <yazeedthunyan@gmail.com>
2024-05-21 18:41:54 +0300

    Lap

---
+++ app.js
@@ -0,0 +1,32 @@
+let rectangle1  = 4
+let rectangle2 = 5
+
+console.log("the area of the rectavgle" + 5*4);
+
+let hot = 50;
+let f = 32 + (1.8*hot);
+console.log(f)
+let fehr = 122
+let ff = 1.8/(32-fehr)
+console.log(ff)
+
+let hour = 1;
+let second = 1*60 * 60;
+
+console.log("NN hours ="+hour+"NN seconds "+second)
+
+let thingone = `ellwwlw`;
+let thingtwo = `keldelel`;
+// console.log(`${thingtwo}+${thingone}`);
+
+var bill = 10.25 + 3.99 + 7.15;
+var tip = bill /0.15; 
+var total = tip;
+console.log(total);
+
+
+let firstName = "yazeed";
+
+let interest ="football";
+let hobby = "gamong"
+let awesomeMessage ="my name is"`${firstName}'"i love"`${interest}`""In my spare time, I like to"${hobby}`
